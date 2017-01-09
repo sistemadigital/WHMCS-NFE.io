81f4e6fff925293d18e016fdd27a02f6991c3838
modulosprontos <luiz@modulosprontos.com.br>
2017-01-09 19:49:07 -0300

    Create webhook.php

---
+++ modules/addons/nfeio/webhook.php
@@ -0,0 +1,29 @@
+<?php
+require_once(dirname(__FILE__)."/../../../includes/hooks/lib/init.php");
+require_once("../../../init.php");
+
+$retorno = file_get_contents('php://input');
+$conteudo = json_decode($retorno);
+
+$sql = mysql_query("SELECT setting, value FROM tbladdonmodules WHERE module = 'nfeio'");
+while($row = mysql_fetch_array($sql)){
+	if ($row['setting'] == "chave_api"):
+		$chaveAPI = $row['value'];
+	elseif ($row['setting'] == "empresa_id"):
+		$empresaID = $row['value'];
+	endif;
+}
+
+if($conteudo->provider->id == $empresaID):
+	if($conteudo->status == "Issued"):
+		NFe::setApiKey($chaveAPI);
+		$url = NFe_ServiceInvoice::pdf(
+			$conteudo->provider->id,
+			$conteudo->id
+		);
+
+		$query = "UPDATE mod_nfeio SET status='{$conteudo->flowStatus}', pdf='{$url}', retorno='{$retorno}' WHERE nf='{$conteudo->id}'";
+		$result = full_query($query);
+	endif;
+endif;
+?>
