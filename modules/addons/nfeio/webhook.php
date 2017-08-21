52ce7d984e8c3611cb35d8fe095afc4b4e3b0133
lpcs007 <lpcs007@gmail.com>
2017-08-21 14:45:21 -0300

    Update webhook.php

--- modules/addons/nfeio/webhook.php
+++ modules/addons/nfeio/webhook.php
@@ -1,23 +1,12 @@
 <?php
-require_once(dirname(__FILE__)."/../../../includes/hooks/lib/init.php");
-require_once("../../../init.php");
+require_once(dirname(__FILE__)."/funcoes.php");
+require_once(dirname(__FILE__)."/../../../init.php");
 
 $retorno = file_get_contents('php://input');
 $conteudo = json_decode($retorno);
 
-$sql = mysql_query("SELECT setting, value FROM tbladdonmodules WHERE module = 'nfeio'");
-while($row = mysql_fetch_array($sql)){
-	if ($row['setting'] == "chave_api"):
-		$chaveAPI = $row['value'];
-	elseif ($row['setting'] == "empresa_id"):
-		$empresaID = $row['value'];
-	endif;
+if($conteudo->provider->id == nfeio_configModulo("empresa_id")){
+	$query = "UPDATE mod_nfeio SET status='".$conteudo->status."', retorno='".serialize($conteudo)."', msg='".$conteudo->flowStatus."' WHERE nf='".$conteudo->id."'";
+	$result = full_query($query);
 }
-
-if($conteudo->provider->id == $empresaID):
-	if($conteudo->status == "Issued"):
-		$query = "UPDATE mod_nfeio SET status='{$conteudo->flowStatus}', retorno='{$retorno}' WHERE nf='{$conteudo->id}'";
-		$result = full_query($query);
-	endif;
-endif;
 ?>
